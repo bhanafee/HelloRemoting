f5f94a175152cbae8ab47503e43741012c04a63e
Brian Hanafee <brian@maybeitssquid.local>
2016-06-29 00:19:15 -0700

    Use Start message on service

--- src/main/scala/service/Main.scala
+++ src/main/scala/service/Main.scala
@@ -5,5 +5,5 @@ import akka.actor.{ActorSystem, Props}
 object Main extends App {
   val system = ActorSystem("ServiceSystem")
   val service = system.actorOf(Props[ServiceActor], name="service")
-  service ! "Wake up"
+  service ! messages.Start
 }
--- src/main/scala/service/ServiceActor.scala
+++ src/main/scala/service/ServiceActor.scala
@@ -9,6 +9,8 @@ class ServiceActor extends Actor {
     s"In ${locale.getDisplayCountry}, ${locale.getDisplayLanguage} is spoken and the currency is the ${Currency.getInstance(locale).getDisplayName}"
 
   override def receive = {
+    case Start =>
+      println("Server is active")
     case Tour(code) =>
       Locale.getAvailableLocales.filter(_.getCountry == code).foreach { locale =>
         sender ! Guide(code, describe(locale))
