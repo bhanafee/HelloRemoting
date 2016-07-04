abc4508fa930d0e341d1eea23b07ef6a4604b0d9
Brian Hanafee <bhanafee@gmail.com>
2016-07-04 13:50:35 -0700

    Reorder messages for readability

--- src/main/scala/messages/package.scala
+++ src/main/scala/messages/package.scala
@@ -1,8 +1,8 @@
 package object messages {
 
-  case class Start(codes: Seq[String])
-
   case class Inquiry(code: String)
 
   case class Guidance(code: String, description: String)
+
+  case class Start(codes: Seq[String])
 }
