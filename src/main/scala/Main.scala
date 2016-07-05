2eb992e5fb5079c9efff77ae66c197012ef764da
Brian Hanafee <bhanafee@gmail.com>
2016-07-04 22:25:20 -0700

    Line breaks for book formatting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,19 @@
 import java.util.Locale
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+
 import guide.GuidebookActor
 import tourist.TouristActor
 
 object Main extends App {
-  val system: ActorSystem = ActorSystem("GuidebookSystem")
+  val system: ActorSystem = ActorSystem("GuideSystem")
 
-  val guideProps: Props = Props(classOf[GuidebookActor], "Wikipedia")
+  val guideProps: Props =
+    Props(classOf[GuidebookActor], "Wikipedia")
   val guidebook: ActorRef = system.actorOf(guideProps)
 
-  val tourProps: Props = Props(classOf[TouristActor], guidebook)
+  val tourProps: Props =
+    Props(classOf[TouristActor], guidebook)
   val tourist: ActorRef = system.actorOf(tourProps)
 
   tourist ! messages.Start(Locale.getISOCountries)
