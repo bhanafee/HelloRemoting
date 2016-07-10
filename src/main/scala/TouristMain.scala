046c00cc886605597323de26e9a3ef4ab9e899c4
Brian Hanafee <bhanafee@gmail.com>
2016-07-09 20:25:38 -0700

    Resolve guidebook actor

--- src/main/scala/TouristMain.scala
+++ src/main/scala/TouristMain.scala
@@ -3,8 +3,22 @@ import java.util.Locale
 import akka.actor.{ActorRef, ActorSystem, Props}
 import tourist.TouristActor
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 object TouristMain extends App {
   val system: ActorSystem = ActorSystem("TouristSystem")
-  val tourist: ActorRef = system.actorOf(Props[TouristActor], name = "tourist")
+
+  val path =
+    "akka.tcp://BookSystem@127.0.0.1:2552/user/guidebook"
+
+  val guidebook: ActorRef = Await.result(system.
+    actorSelection(path).
+    resolveOne(5 seconds), 6 seconds)
+
+  val tourProps: Props =
+    Props(classOf[TouristActor], guidebook)
+  val tourist: ActorRef = system.actorOf(tourProps)
+
   tourist ! messages.Start(Locale.getISOCountries)
 }
