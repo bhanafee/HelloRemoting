7e98bd28ff733925c322f55966a2699053d14c6c
Brian Hanafee <hanafebj@wellsfargo.com>
2016-07-10 16:41:22 -0700

    Use onComplete instead of Await

--- src/main/scala/TouristMain.scala
+++ src/main/scala/TouristMain.scala
@@ -1,24 +1,29 @@
 import java.util.Locale
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.util.Timeout
 import tourist.TouristActor
 
-import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object TouristMain extends App {
+  implicit val timeout: Timeout = 5 seconds
+
   val system: ActorSystem = ActorSystem("TouristSystem")
 
   val path =
     "akka.tcp://BookSystem@127.0.0.1:2552/user/guidebook"
 
-  val guidebook: ActorRef = Await.result(system.
-    actorSelection(path).
-    resolveOne(5 seconds), 5 seconds)
+  system.actorSelection(path).resolveOne().onComplete {
+    case Success(guidebook) =>
+      val tourProps: Props =
+        Props(classOf[TouristActor], guidebook)
+      val tourist: ActorRef = system.actorOf(tourProps)
 
-  val tourProps: Props =
-    Props(classOf[TouristActor], guidebook)
-  val tourist: ActorRef = system.actorOf(tourProps)
+      tourist ! messages.Start(Locale.getISOCountries)
 
-  tourist ! messages.Start(Locale.getISOCountries)
+    case Failure(e) => println(e)
+  }
 }
