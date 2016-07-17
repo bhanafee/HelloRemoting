655831c4949c20d779ce86eb695086beec0a018e
Brian Hanafee <bhanafee@gmail.com>
2016-07-17 12:42:16 -0700

    Add local pool to guidebook

--- src/main/scala/GuidebookMain.scala
+++ src/main/scala/GuidebookMain.scala
@@ -1,9 +1,10 @@
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.routing.FromConfig
 
 object GuidebookMain extends App {
   val system: ActorSystem = ActorSystem("BookSystem")
 
   val guideProps: Props = Props[Guidebook]
   val guidebook: ActorRef =
-    system.actorOf(guideProps, "guidebook")
+    system.actorOf(FromConfig.props(guideProps), "guidebook")
 }
